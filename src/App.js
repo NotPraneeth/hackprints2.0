080333ae52baec9a6d311f7c8ecf7d6dac9e1f4e
NotPraneeth <rsphaha@gmail.com>
2023-02-25 23:43:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+
+import "./fonts/Yustika.ttf"
+import "./fonts/Yustika.woff"
+import "./fonts/Yustika.woff2"
+
+
+import Landing from './Components/Landing/Landing';
+import Navbar from './Components/Navbar/Navbar';
+import Wow from './Components/Wow/Wow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Landing/>
+      <Wow/>
     </div>
   );
 }
