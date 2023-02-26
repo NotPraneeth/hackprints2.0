4d2192c818f6997e9a5eedcce233e35d9b5a48b5
NotPraneeth <rsphaha@gmail.com>
2023-02-26 17:19:24 +0530

    fixed font weight issue

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div className='contact-container'>
         <div className="contact-main">
-            <div className="contact-hero"><span className='first-line'>NOT CONVICED?</span> <br></br> REACH OUT TO US AND YOU WILL BE.</div>
+            <div className="contact-hero"><span className='first-line orange'>NOT <span className='orange'>CONVICED?</span> </span> <br></br> REACH OUT TO US AND YOU WILL BE.</div>
             <div className="instagram">INSTAGRAM</div>
             <div className="email">EMAIL</div>
             <div className="twitter">TWITTER</div>
